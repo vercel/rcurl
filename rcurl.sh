c8de4a78d23853b29b3e0bc8bf66ab385d42d3e9
Nathan Rajlich <nathan@tootallnate.net>
2018-11-21 11:44:56 -0800

    Fix rcurl debug when `-v` is part of the URL

--- rcurl.sh
+++ rcurl.sh
@@ -1,7 +1,13 @@
 import "dns@1.0.0"
 
+rcurl__debug() {
+  echo "$@" | grep -w '\-v' >/dev/null || echo "$@" | grep -w '\-\-verbose' >/dev/null
+}
+
 rcurl_debug() {
-  [ ! -z "$(echo "$*" | grep '\-v' ||:)" ] && echo '* rcurl -' "$@" >&2 ||:
+  local msg="$1"
+  shift
+  rcurl__debug "$@" && echo "* rcurl - $msg" >&2 ||:
 }
 
 rcurl() {
@@ -12,21 +18,21 @@ rcurl() {
   shift
 
   hostname="$(echo "${url}" | sed 's~http[s]*://~~g' | sed 's/\/.*//')"
-  rcurl_debug 'Hostname: ' "${hostname}"
+  rcurl_debug "Hostname: ${hostname}" "$@"
 
   port=80
   if echo "${url}" | grep -E -i '^https://' >/dev/null; then
     port=443
   fi
-  rcurl_debug 'Port:     ' "${port}"
+  rcurl_debug "Port:     ${port}" "$@"
 
   target_ip="$(dns_lookup "${target}")"
   if [ -z "${target_ip}" ]; then
     # if dig returned nothing then is 127.0.0.1 or something
     target_ip="${target}"
   fi
-  rcurl_debug 'Target IP:' "${target_ip}"
+  rcurl_debug "Target IP: ${target_ip}" "$@"
 
-  rcurl_debug 'Command:  ' curl --resolve "${hostname}:${port}:${target_ip}" "${url}" "$@"
+  rcurl_debug "Command:   curl --resolve ${hostname}:${port}:${target_ip} ${url} $*" "$@"
   curl --resolve "${hostname}:${port}:${target_ip}" "${url}" "$@"
 }
